60f017e9d570ec298925f7cfed990cbcf639db18
MohZaib-Ali <mr.mohzaib.ali@gmail.com>
2022-06-10 14:08:59 +0500

    Minor changes

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,9 +4,11 @@ import { UserDocument } from "../db/models/User";
 
 declare namespace Express {
   export interface Request {
-      user: UserDocument;
+      user?: UserDocument;
+      token?: string;
   }
   export interface Response {
-      user: UserDocument;
+      user?: UserDocument;
+      token?: string;
   }
 }
